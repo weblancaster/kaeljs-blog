e4041121d5e5bba4c31a369b3bb15900caed51c0
Michael Lancaster <free005304@amchiFRL0005304.local>
2014-02-20 17:23:17 -0600

    add event to show aside, add about and lab page, add updates/improvements to the theme

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,12 @@
  * GET home page.
  */
 
-var fs = require('fs');
-
+// List of articles title
 exports.listArticles = function(db) {
     return function(req, res){
         db.find({}, function (error, docs) {
             if ( error ) {
-                return new Error('Error on getting list of Articles');
+                return new Error('Error trying to get list of article', error);
             }
 
             res.render('index', {
@@ -21,21 +20,65 @@ exports.listArticles = function(db) {
     }
 }
 
+// Article post
 exports.article = function(db) {
     return function(req, res){
         var pagePath = req.param('title');
 
-        db.findOne({ path: pagePath }, function (error, doc) {
+        db.find({}, function (error, docs) {
             if ( error ) {
-                return new Error('Error on getting article');
+                return new Error('Error trying to get list of article', error);
             }
 
-            res.render('blog', {
-                    title: doc.title,
-                    content: doc
+            db.findOne({ path: pagePath }, function (error, doc) {
+                if ( error ) {
+                    return new Error('Error trying to get article', error);
+                }
+
+                res.render('blog', {
+                        title: doc.title,
+                        content: doc,
+                        articles: docs
+                    }
+                );
+            });
+
+        });
+    }
+}
+
+// About page
+exports.about = function(db) {
+    return function(req, res){
+        db.find({}, function (error, docs) {
+            if ( error ) {
+                return new Error('Error trying to get list of article', error);
+            }
+
+            res.render('about', {
+                    title: 'About page title',
+                    articles: docs
                 }
             );
-        });
 
+        });
+    }
+}
+
+// Lab page
+exports.lab = function(db) {
+    return function(req, res){
+        db.find({}, function (error, docs) {
+            if ( error ) {
+                return new Error('Error trying to get list of article', error);
+            }
+
+            res.render('lab', {
+                    title: 'Lab page title',
+                    articles: docs
+                }
+            );
+
+        });
     }
 }
